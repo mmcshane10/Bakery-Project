e04a1fd434a925277c77e4f98e98a357cd1d085a
Mike <mmcshane10@gmail.com>
2019-09-27 15:30:29 -0700

    moved remaining program consoles to a backend method.

--- Bakery/UI.cs
+++ Bakery/UI.cs
@@ -5,6 +5,57 @@ namespace Bakery.Order
 {
     public class Script
     {
+
+        public static void MainMenu()
+        {
+            Console.WriteLine("-----------------------------------------------------------");
+            Console.WriteLine("**Welcome to EpiDoughdus!**");
+            Console.WriteLine("Please select one of the below options:");
+            Console.WriteLine("1. Place an Order");
+            Console.WriteLine("2. Complain About The Pun Made Above");
+            string mainMenuInput = Console.ReadLine();
+
+            if (mainMenuInput == "1")
+            {
+                Console.WriteLine("-----------------------------------------------------------");
+                Console.WriteLine("**Here at EpiDoughdus we offer two exquisite products: our world famous BREAD and our instagram-worthy PASTRIES!**");
+                Console.WriteLine("Please select one of the below options:");
+                Console.WriteLine("1. Gimme that delicious gluten");
+                Console.WriteLine("2. I'm scared and want to go back");
+                string beginOrderInput = Console.ReadLine();
+
+                if (beginOrderInput == "1")
+                {
+                    Console.WriteLine("-----------------------------------------------------------");
+                    Console.WriteLine("**Bread is $5/loaf. Every third loaf you purchase is FREE**");
+                    Console.WriteLine("**Pastries are $2 each or 3 for $5**");
+                    Script.BeginOrder();
+                    Script.ConfirmOrder();
+                }
+                else if (beginOrderInput == "2")
+                {
+                    MainMenu();
+                }
+                else
+                {
+                    Console.WriteLine("-----------------------------------------------------------");
+                    Console.WriteLine("**Please enter a valid number**");
+                    MainMenu();
+                }
+            }
+            else if (mainMenuInput == "2")
+            {
+                Console.WriteLine("-----------------------------------------------------------");
+                Console.WriteLine("Please direct all complaints to elly@epidoughdus.com");
+                MainMenu();
+            }
+            else{
+                Console.WriteLine("-----------------------------------------------------------");
+                Console.WriteLine("**Please enter a valid number**");
+                MainMenu();
+            }
+        }
+
         public static void BeginOrder()
         {
         Console.WriteLine("Please enter the number of BREADS you would like to order:");
@@ -34,9 +85,9 @@ namespace Bakery.Order
             if (confirmOrderInput == "1")
             {
                 Console.WriteLine("-----------------------------------------------------------");
-                Console.WriteLine("Thank you for your order! [press any key to return to the Main Menu]");
+                Console.WriteLine("Thank you for ordering from EpiDoughdus! [press any key to return to the Main Menu]");
                 Console.ReadKey();
-                Program.Main();
+                MainMenu();
             }
             else if (confirmOrderInput == "2")
             {
--- Program.cs
+++ Program.cs
@@ -7,52 +7,7 @@ namespace Bakery.Order
     {
         public static void Main()
         {
-            Console.WriteLine("-----------------------------------------------------------");
-            Console.WriteLine("**Welcome to Pierre's Artisan Bakery!**");
-            Console.WriteLine("Please select one of the below options:");
-            Console.WriteLine("1. Place an Order");
-            Console.WriteLine("2. Submit a Complaint");
-            string mainMenuInput = Console.ReadLine();
-
-            if (mainMenuInput == "1")
-            {
-                Console.WriteLine("-----------------------------------------------------------");
-                Console.WriteLine("**Here at Pierre's we offer two exquisite products: our world famous BREAD and our instagram-worthy PASTRIES!**");
-                Console.WriteLine("Please select one of the below options:");
-                Console.WriteLine("1. Gimme that delicious gluten");
-                Console.WriteLine("2. I'm scared and want to go back");
-                string beginOrderInput = Console.ReadLine();
-
-                if (beginOrderInput == "1")
-                {
-                    Console.WriteLine("-----------------------------------------------------------");
-                    Console.WriteLine("**Bread is $5/loaf. Every third loaf you purchase is FREE**");
-                    Console.WriteLine("**Pastries are $2 each or 3 for $5**");
-                    Script.BeginOrder();
-                    Script.ConfirmOrder();
-                }
-                else if (beginOrderInput == "2")
-                {
-                    Main();
-                }
-                else
-                {
-                    Console.WriteLine("-----------------------------------------------------------");
-                    Console.WriteLine("**Please enter a valid number**");
-                    Main();
-                }
-            }
-            else if (mainMenuInput == "2")
-            {
-                Console.WriteLine("-----------------------------------------------------------");
-                Console.WriteLine("Please direct all complaints to elly@epicodus.com");
-                Main();
-            }
-            else{
-                Console.WriteLine("-----------------------------------------------------------");
-                Console.WriteLine("**Please enter a valid number**");
-                Main();
-            }
+            Script.MainMenu();
         }
     }
 }
